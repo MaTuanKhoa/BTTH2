5db8a88aed8c40a5be18d567f14f0fb8177b95d2
MaTuanKhoa <147571785+MaTuanKhoa@users.noreply.github.com>
2023-10-16 00:37:41 +0700

    Add files via upload

---
+++ bài 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"nhap 2 so a va b ";
+	cin>>a>>b;
+
+	if (a>b) {cout<<"so lon hon la: a = "<<a;}
+	else if (a==b) {cout<<"a = b";}
+	else {cout<<"so lon hon la: b = "<<b;}
+} 
